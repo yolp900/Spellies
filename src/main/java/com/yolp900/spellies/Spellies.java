4925a57ceda6e0e7ed91d9e97f75aed4a2d0ca3c
yolp900 <yonatan.assaf@gmail.com>
2019-10-15 01:58:45 +0300

    Initial commit

---
+++ src/main/java/com/yolp900/spellies/Spellies.java
@@ -0,0 +1,10 @@
+package com.yolp900.spellies;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("spellies")
+public class Spellies {
+    public Spellies() {
+        // TODO: Make a mod.
+    }
+}
